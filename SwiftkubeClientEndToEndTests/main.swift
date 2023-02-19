5ccd38503686e484f6e42fd498889f458450b72f
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2023-02-19 18:26:35 +0000

    Basic tests

--- SwiftkubeClientEndToEndTests/main.swift
+++ SwiftkubeClientEndToEndTests/main.swift
@@ -6,6 +6,24 @@
 //
 
 import Foundation
+import SwiftkubeClient
 
-print("Hello, World!")
+if let client = KubernetesClient() {
 
+    print("deployments:")
+    let deployments = try await client.appsV1.deployments.list(in: .default)
+    for deployment in deployments {
+        print(deployment.name ?? "dumb dumb super error")
+    }
+    
+    print("cronjobs:")
+    let cronjobs = try await client.batchV1.cronJobs.list(in: .default)
+    for cronjob in cronjobs {
+        print(cronjob.name ?? "dumb dumb super error")
+        let newJob = try cronjob.generateJob()
+        let r = try await client.batchV1.jobs.create(in: .default, newJob)
+        print(r.name!)
+    }
+    
+    try client.syncShutdown()
+}
